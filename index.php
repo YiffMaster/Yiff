47cd37906b1a39123540e0339b87730a230b0a76
YiffMaster <yiffmaster47@gmail.com>
2017-04-03 22:23:50 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <title>YiffMaster - YiffHaven On Discord</title>
-<link rel="shortcut icon" href="icon.png" type="image/x-icon">
+<link rel="shortcut icon" href="https://yiffmaster.github.io/Yiff/icon.png" type="image/x-icon">
 </head>
 <body>
 <iframe style="float: left;" src="https://discordapp.com/widget?id=298639734396289065&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
